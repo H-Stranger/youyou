f1811891981452d733e29730d25ef2ed92b99d35
H-Stranger <3547845923@qq.com>
2026-03-30 22:00:11 +0800

    后端项目初始化

---
+++ youyou-backend-master/src/main/java/com/yupi/project/MyApplication.java
@@ -0,0 +1,15 @@
+package com.hxd.project;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.hxd.project.mapper")
+public class MyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyApplication.class, args);
+    }
+
+}
---
+++ youyou-backend-master/src/main/java/com/yupi/project/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.hxd.project.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.hxd.project.model.entity.User;
+
+/**
+ * @Entity com.hxd.project.model.domain.User
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
